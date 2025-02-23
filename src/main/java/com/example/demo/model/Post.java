79f45ec77c400d75f65a2f3f2292c2bccbd92f10
sdybybhon <artem563732@mail.ru>
2025-02-23 20:37:42 +0500

    Bootstrap connection and mobile integration

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,12 +1,15 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text, Integer likes) {
+    public Post(String text, Date date) {
         this.text = text;
-        this.likes = likes;
+        this.creationDate = date;
     }
 
     public String getText() {
@@ -15,5 +18,9 @@ public class Post {
 
     public Integer getLikes() {
         return likes;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -2,15 +2,16 @@ package com.example.demo.services;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> ListAllPosts() {
         List<Post> list = new ArrayList<>();
-        list.add(new Post("Нас 25000", 1));
-        list.add(new Post("И мы идём",15));
-        list.add(new Post("Сдавать джаву", 15));
+        list.add(new Post("Нас 25000", new Date()));
+        list.add(new Post("И мы идём",new Date()));
+        list.add(new Post("Сдавать джаву", new Date()));
         return list;
     }
 }
